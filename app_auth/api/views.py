e8d27f6f0deca75184c214e13365066060fbbd4b
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-25 13:31:21 +0100

    Add login view

--- app_auth/api/views.py
+++ app_auth/api/views.py
@@ -36,3 +36,13 @@ class ActivateAccountView(APIView):
             return Response({'message': result['message']}, status=status.HTTP_200_OK)
         else:
             return Response({'error': result['error']}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    """POST /api/login/"""
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
