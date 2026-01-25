25eee7cc75cfa68553fef0262ce85b036d1bc425
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-25 19:34:49 +0000

    Fix url path

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
-    path('api/', include('app_auth.urls')),
+    path('api/', include('app_auth.api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
