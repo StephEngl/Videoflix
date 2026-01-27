cac97160d08aa56454f69dba7edbf48d656c9128
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-27 16:19:36 +0000

    Change activate account view from response to redirect

--- app_auth/api/views.py
+++ app_auth/api/views.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.core.mail import send_mail
+from django.shortcuts import redirect
 from django.conf import settings
 from .serializers import RegistrationSerializer, LoginSerializer
 from ..services import EmailService, AuthService
@@ -46,9 +46,9 @@ class ActivateAccountView(APIView):
         result = AuthService.activate_user(uidb64, token)
 
         if result['success']:
-            return Response({'message': result['message']}, status=status.HTTP_200_OK)
+            return redirect(f"{settings.FRONTEND_URL}?activation=success")
         else:
-            return Response({'error': result['error']}, status=status.HTTP_400_BAD_REQUEST)
+            return redirect(f"{settings.FRONTEND_URL}?activation=error")
 
 
 class LoginView(APIView):
