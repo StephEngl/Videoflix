311da78a0df5312262e97c7799ed08e9a1237528
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-27 16:05:49 +0000

    Add attach logo to email to email service

--- app_auth/services.py
+++ app_auth/services.py
@@ -1,10 +1,13 @@
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from email.mime.image import MIMEImage
+import os
 
 User = get_user_model()
 
@@ -17,27 +20,23 @@ class EmailService:
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         activation_link = f"{settings.FRONTEND_URL}/activate/{uid}/{token}/"
         
-        # Render HTML template
-        html_content = render_to_string('app_auth/templates/confirm_email.html', {
+        # Content ID for embedded logo
+        logo_cid = "logo"
+        
+        context = {
             'username': user.username,
             'activation_link': activation_link,
-        })
+            'logo_cid': logo_cid,
+            'base_url': settings.FRONTEND_URL,
+        }
+
+         # Plain text fallback
+        text_content = render_to_string('confirm_email.txt', context)
+
+        # Render HTML template
+        html_content = render_to_string('confirm_email.html', context)
         
-        # Plain text fallback
-        text_content = f"""
-        Dear {user.username},
-        
-        Thank you for registering at Videoflix!
-        Please click the following link to verify your email address and complete your registration:
-        {activation_link}
-        
-        If you didn't create this account, please disregard this email.
-        
-        Best regards,
-        Your Videoflix Team
-        """
-        
-        # Send email
+        # Create email message
         email_message = EmailMultiAlternatives(
             subject="Activate your Videoflix account",
             body=text_content,
@@ -45,9 +44,25 @@ class EmailService:
             to=[user.email]
         )
         email_message.attach_alternative(html_content, "text/html")
+        
+        EmailService.attach_logo_to_email(email_message, logo_cid)
+        
         email_message.send(fail_silently=False)
         
         return token
+
+    @staticmethod
+    def attach_logo_to_email(email_message, logo_cid):
+        """Attach logo image to email message."""
+        logo_path = os.path.join(settings.BASE_DIR, 'app_auth', 'assets', 'logo_icon.svg')
+        if os.path.exists(logo_path):
+            with open(logo_path, 'rb') as f:
+                logo_data = f.read()
+            
+            logo_image = MIMEImage(logo_data, _subtype='svg+xml')
+            logo_image.add_header('Content-ID', f'<{logo_cid}>')
+            logo_image.add_header('Content-Disposition', 'inline', filename='logo.svg')
+            email_message.attach(logo_image)
     
 
 class AuthService:
