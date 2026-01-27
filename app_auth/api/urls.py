9084456e77b83b0f6782424b40fe36f4c8ad7ab8
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-27 16:00:51 +0000

    Add token refresh and test email endpoint to auth-urls

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegistrationView, ActivateAccountView, LoginView, LogoutView, TokenRefreshView
+from .views import RegistrationView, ActivateAccountView, LoginView, LogoutView, CookieTokenRefreshView, TestEmailView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('activate/<str:uidb64>/<str:token>/', ActivateAccountView.as_view(), name='activate'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('test-email/', TestEmailView.as_view(), name='test_email'),
 ]
