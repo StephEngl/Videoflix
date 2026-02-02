868ddc24b85961b84a48b8a2dff01a3be084e2e8
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-02 09:43:31 +0000

    Add password reset and confirm urls

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import RegistrationView, ActivateAccountView, LoginView, LogoutView, CookieTokenRefreshView, TestEmailView
+from .views import (
+    RegistrationView, 
+    ActivateAccountView, 
+    LoginView, 
+    LogoutView, 
+    CookieTokenRefreshView, 
+    PasswordResetView, 
+    PasswordConfirmView,
+    TestEmailView
+)
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
@@ -7,5 +16,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_confirm/<str:uidb64>/<str:token>/', PasswordConfirmView.as_view(), name='password_confirm'),
     path('test-email/', TestEmailView.as_view(), name='test_email'),
 ]
