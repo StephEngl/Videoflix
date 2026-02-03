b25e02b015fba8449d268a3589ac8b6a2ec17aef
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-03 21:27:31 +0000

    Register video functions at the admin panel

--- app_video/admin.py
+++ app_video/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'processing_status', 'is_processed', 'created_at')
+    list_filter = ('processing_status', 'category', 'is_processed')
+    readonly_fields = ('processing_status', 'is_processed', 'hls_480p_path', 'hls_720p_path', 'hls_1080p_path')
