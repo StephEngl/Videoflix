a21f0565bf96316113cb67baaf9b7cdbca92dbd4
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-04 11:45:25 +0000

    Set thumbnail url at video serializer

--- app_video/api/serializers.py
+++ app_video/api/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 
 
 class VideoSerializer(serializers.ModelSerializer):
-    
+    thumbnail_url = serializers.SerializerMethodField()
+    def get_thumbnail_url(self, obj):
+        if obj.thumbnail:
+                request = self.context.get('request')
+                if request:
+                    return request.build_absolute_uri(obj.thumbnail.url)
+                return obj.thumbnail.url
+        return None
+
     class Meta:
         model = Video
         fields = ['id', 'created_at', 'title', 'description', 'thumbnail_url', 'category']
