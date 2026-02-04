c5fe7c316cca9174885befecd15d7470ec1d0f7e
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-04 11:44:51 +0000

    Change thumbnail charfield to filefield

--- app_video/models.py
+++ app_video/models.py
@@ -13,7 +13,7 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    thumbnail_url = models.URLField(max_length=500, blank=True, null=True)
+    thumbnail = models.FileField(upload_to='videos/hls/', blank=True, null=True)
     category = models.CharField(max_length=100)
 
     original_video = models.FileField(upload_to='videos/original/')
