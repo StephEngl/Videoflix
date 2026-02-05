79c2d0e9759da21bacfec416de5f55ca753f790b
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-05 12:46:36 +0100

    Change thumbnail folder

--- app_video/models.py
+++ app_video/models.py
@@ -13,7 +13,7 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    thumbnail = models.FileField(upload_to='videos/hls/', blank=True, null=True)
+    thumbnail = models.FileField(upload_to='videos/thumbnails/', blank=True, null=True)
     category = models.CharField(max_length=100)
 
     original_video = models.FileField(upload_to='videos/original/')
