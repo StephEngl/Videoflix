23fbeef86c89f5ebdbad9a4acadd9cd970c27867
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-10 14:09:53 +0000

    Add queueing order to signals

--- app_video/signals.py
+++ app_video/signals.py
@@ -2,6 +2,7 @@ from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Video
+from .tasks import convert_video_to_hls, create_thumbnail, create_master_playlist
 
 import django_rq
 
@@ -19,17 +20,23 @@ def video_post_save_handler(sender, instance, created, **kwargs):
     """
     if created and instance.original_video:
         def enqueue_video_processing():
-            queue = django_rq.get_queue('default', autocommit=True)
+            default_queue = django_rq.get_queue('default', autocommit=True)
 
+            # Alle HLS-Konvertierungen
+            previous_job = None
             for format_data in VIDEO_RESOLUTIONS:
-                from .tasks import convert_video_to_hls
-                queue.enqueue(convert_video_to_hls, instance.id, *format_data)
+                job = default_queue.enqueue(
+                    convert_video_to_hls, 
+                    instance.id, 
+                    *format_data,
+                    depends_on=previous_job
+                )
+                previous_job = job
             
             if not instance.thumbnail:
-                from .tasks import create_thumbnail
-                queue.enqueue(create_thumbnail, instance.id)
+                default_queue.enqueue(create_thumbnail, instance.id)
             
-            from .tasks import create_master_playlist
-            queue.enqueue(create_master_playlist, instance.id)
+            # Master-Playlist erstellen
+            default_queue.enqueue(create_master_playlist, instance.id, depends_on=previous_job)
 
         transaction.on_commit(enqueue_video_processing)
