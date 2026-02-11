4ba9e2256cdd97bcf67f934d412218fbe4562179
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-11 10:31:08 +0000

    Add video post delete signal

--- app_video/signals.py
+++ app_video/signals.py
@@ -1,8 +1,10 @@
+import os
+from django.conf import settings
 from django.db import transaction
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Video
-from .tasks import convert_video_to_hls, create_thumbnail, create_master_playlist
+from .tasks import convert_video_to_hls, create_thumbnail, create_master_playlist, cleanup_deleted_video_files
 
 import django_rq
 
@@ -12,17 +14,22 @@ VIDEO_RESOLUTIONS = [
     ("1080p", "1920:1080", "5000k", "192k"),
 ]
 
+
 @receiver(post_save, sender=Video)
 def video_post_save_handler(sender, instance, created, **kwargs):
-    """
-    Signal handler for video processing.
-    Starts processing job when new video is uploaded.
+    """Handle video upload and start processing pipeline.
+    
+    Automatically starts video processing tasks when a new video is uploaded.
+    Processing includes HLS conversion in multiple resolutions, thumbnail
+    creation, and master playlist generation.
+        
+    Returns:
+        None: Function schedules background tasks and returns.
     """
     if created and instance.original_video:
         def enqueue_video_processing():
             default_queue = django_rq.get_queue('default', autocommit=True)
 
-            # Alle HLS-Konvertierungen
             previous_job = None
             for format_data in VIDEO_RESOLUTIONS:
                 job = default_queue.enqueue(
@@ -36,7 +43,41 @@ def video_post_save_handler(sender, instance, created, **kwargs):
             if not instance.thumbnail:
                 default_queue.enqueue(create_thumbnail, instance.id)
             
-            # Master-Playlist erstellen
             default_queue.enqueue(create_master_playlist, instance.id, depends_on=previous_job)
 
         transaction.on_commit(enqueue_video_processing)
+
+
+@receiver(post_delete, sender=Video)
+def video_post_delete_handler(sender, instance, **kwargs):
+    """Handle video deletion and cleanup associated files.
+    
+    Automatically cleans up all video-related files and directories
+    when a video instance is deleted from the database. This includes
+    original video files, thumbnails, and HLS conversion outputs.
+        
+    Returns:
+        None: Function schedules background cleanup task and returns.
+    """
+    def enqueue_video_cleanup():
+        file_paths = []
+        
+        if instance.original_video:
+            original_path = os.path.join(settings.MEDIA_ROOT, instance.original_video.name)
+            file_paths.append(original_path)
+        
+        if instance.thumbnail:
+            thumbnail_path = os.path.join(settings.MEDIA_ROOT, instance.thumbnail.name)
+            file_paths.append(thumbnail_path)
+        
+        hls_dir = os.path.join(settings.MEDIA_ROOT, 'videos', 'hls', str(instance.id))
+        file_paths.append(hls_dir)
+        
+        thumbnail_dir = os.path.join(settings.MEDIA_ROOT, 'videos', 'thumbnails', str(instance.id))
+        file_paths.append(thumbnail_dir)
+        
+        if file_paths:
+            default_queue = django_rq.get_queue('default', autocommit=True)
+            default_queue.enqueue(cleanup_deleted_video_files, instance.id, file_paths)
+    
+    transaction.on_commit(enqueue_video_cleanup)
