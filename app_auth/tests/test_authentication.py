fcd017812d0f01d42a6e6d08b2a6d94dc57d5b05
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-16 10:47:38 +0000

    Remove dead code

--- app_auth/tests/test_authentication.py
+++ app_auth/tests/test_authentication.py
@@ -1,9 +1,6 @@
 import pytest
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.contrib.auth.tokens import default_token_generator
 from rest_framework import status
 
 User = get_user_model()
