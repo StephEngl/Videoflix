25e40cb9d99c5e87a1cce546171c6d2d4ce6ff9c
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-18 15:18:40 +0000

    Adapt queuing order and delete handler

--- app_video/signals.py
+++ app_video/signals.py
@@ -1,8 +1,7 @@
-import os
-from django.conf import settings
 from django.db import transaction
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django_rq import get_queue
 from .models import Video
 from .tasks import convert_video_to_hls, create_thumbnail, create_master_playlist, cleanup_deleted_video_files
 
@@ -31,17 +30,16 @@ def video_post_save_handler(sender, instance, created, **kwargs):
             default_queue = django_rq.get_queue('default', autocommit=True)
 
             previous_job = None
+            if not instance.thumbnail:
+                default_queue.enqueue(create_thumbnail, instance.id)
+            
             for format_data in VIDEO_RESOLUTIONS:
                 job = default_queue.enqueue(
                     convert_video_to_hls, 
                     instance.id, 
                     *format_data,
-                    depends_on=previous_job
                 )
                 previous_job = job
-            
-            if not instance.thumbnail:
-                default_queue.enqueue(create_thumbnail, instance.id)
             
             default_queue.enqueue(create_master_playlist, instance.id, depends_on=previous_job)
 
@@ -50,34 +48,13 @@ def video_post_save_handler(sender, instance, created, **kwargs):
 
 @receiver(post_delete, sender=Video)
 def video_post_delete_handler(sender, instance, **kwargs):
-    """Handle video deletion and cleanup associated files.
+    """
+    Handle video deletion and cleanup associated files.
     
-    Automatically cleans up all video-related files and directories
-    when a video instance is deleted from the database. This includes
-    original video files, thumbnails, and HLS conversion outputs.
-        
-    Returns:
-        None: Function schedules background cleanup task and returns.
+    With new structure: everything in media/videos/{id}/
     """
     def enqueue_video_cleanup():
-        file_paths = []
-        
-        if instance.original_video:
-            original_path = os.path.join(settings.MEDIA_ROOT, instance.original_video.name)
-            file_paths.append(original_path)
-        
-        if instance.thumbnail:
-            thumbnail_path = os.path.join(settings.MEDIA_ROOT, instance.thumbnail.name)
-            file_paths.append(thumbnail_path)
-        
-        hls_dir = os.path.join(settings.MEDIA_ROOT, 'videos', 'hls', str(instance.id))
-        file_paths.append(hls_dir)
-        
-        thumbnail_dir = os.path.join(settings.MEDIA_ROOT, 'videos', 'thumbnails', str(instance.id))
-        file_paths.append(thumbnail_dir)
-        
-        if file_paths:
-            default_queue = django_rq.get_queue('default', autocommit=True)
-            default_queue.enqueue(cleanup_deleted_video_files, instance.id, file_paths)
+        default_queue = get_queue('default', autocommit=True)
+        default_queue.enqueue(cleanup_deleted_video_files, instance.id) 
     
     transaction.on_commit(enqueue_video_cleanup)
