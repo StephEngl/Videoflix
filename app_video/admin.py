adb56c0e19a4318db38659cb39da9140fe4b87ce
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-18 15:16:56 +0000

    Deactivate thumbnail-field at admin

--- app_video/admin.py
+++ app_video/admin.py
@@ -12,4 +12,4 @@ class VideoAdmin(admin.ModelAdmin):
     """
     list_display = ('title', 'category', 'processing_status', 'is_processed', 'created_at')
     list_filter = ('processing_status', 'category', 'is_processed')
-    readonly_fields = ('processing_status', 'is_processed', 'hls_480p_path', 'hls_720p_path', 'hls_1080p_path')
+    readonly_fields = ('thumbnail', 'processing_status', 'is_processed', 'hls_480p_path', 'hls_720p_path', 'hls_1080p_path')
