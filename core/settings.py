824d30c0f8b38eedf5e32d676e11f50862393b0f
Stephanie Englberger <stephaniekoebich@aol.com>
2026-02-19 11:52:40 +0100

    Adapt JWT token lifetime

--- core/settings.py
+++ core/settings.py
@@ -198,8 +198,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=60),
 }
 
 SPECTACULAR_SETTINGS = {
